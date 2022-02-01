4e314d068a0204d4eb510cc3c2bf38af98b4aba3
Erwan de Belleville <edebelleville@hotmail.fr>
2022-02-01 10:39:32 +0100

    signin error

--- utils/error.utils.js
+++ utils/error.utils.js
@@ -5,17 +5,14 @@ module.exports.signUpErrors = (err) =>{
         errors.pseudo = "Votre pseudo est incorrect";
     }
  
-    
     if(err.message.includes('email')){
         errors.pseudo = "Votre email est incorrect";
     }
 
-    
     if(err.message.includes('password')){
-        errors.pseudo = "password trop court !";
+        errors.pseudo = "mot de passe trop court !";
     }
   
-    
     if(err.code === 11000 & Object.keys(err.keyValue)[0].includes("pseudo")){
         errors.pseudo = "Ce pseudo est déjà pris";
     }
@@ -24,6 +21,19 @@ module.exports.signUpErrors = (err) =>{
         errors.pseudo = "Cet email est déjà utilisé";
     }
     
+    return errors;
+}
+
+module.exports.signInErrors = (err) => {
+    let errors = {email:"", password:""};
+
+    if(err.message.includes("email")){
+        errors.email = "Email inconnu";
+    }
+
+    if(err.message.includes("password")){
+        errors.email = "ce mot de passe ne correspond pas";
+    }
 
     return errors;
 }
