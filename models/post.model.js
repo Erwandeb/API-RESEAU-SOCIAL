3dcd6d0c09918fd1f5400358fc97fa93668fa11b
Erwan de Belleville <edebelleville@hotmail.fr>
2022-02-01 11:03:14 +0100

    post model

---
+++ models/post.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const PostSchema = new mongoose.Schema(
+    {
+        posterId :{
+            type: String,
+            required: true
+        },
+        message: {
+                type: String,
+                trim : true,
+                maxlength: 400
+        },
+        picture:{
+            type: String,
+        },
+        video:{
+            type: String
+        },
+        likers:{
+            type:[String],
+            require:true,
+        },
+        comments :{
+            type:[
+                {
+                    commenterId : String,
+                    commenterPseudo :String,
+                    text : String,
+                    timestamp : Number,
+                }
+            ],
+            required : true,
+        },
+    },
+    {
+        timestamp : true
+    },
+)
+
+module.exports = mongoose.model('post', PostSchema);
