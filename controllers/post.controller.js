629ac7fdd886d7ea8f7198ecf31de9fe8a1949e1
Erwan de Belleville <edebelleville@hotmail.fr>
2022-02-03 09:27:39 +0100

    createPost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,22 +4,34 @@ const objectID = require('mongoose').Types.ObjectId;
 
 
 
-module.exports.readPost = (req, red) => {
+module.exports.readPost = (req, res) => {
     postModel.find((err, docs) => {
         if(!err) res.send(docs);
-        else console.log('Error');
-        
+        else console.log('Impossible de retrouver les données' + err);
+
     })
 }
 
-module.exports.createPost = (req, red) => {
+module.exports.createPost = async (req, res) => {
+    const newPost = new postModel({
+        posterId : req.body.posterId,
+        message : req.body.message,
+        video : req.body.video,
+        likers : [],
+        comments : [],
+    });
+    try{
+        const post = await newPost.save();
+        return res.status(201).json(post);
+    }catch(err){
+        return res.status(400).send("Erreur dans createPost" + err)
+    }
+}
+
+module.exports.updatePost = (req, res) => {
     
 }
 
-module.exports.updatePost = (req, red) => {
-    
-}
-
-module.exports.deletePost = (req, red) => {
+module.exports.deletePost = (req, res) => {
     
 }
