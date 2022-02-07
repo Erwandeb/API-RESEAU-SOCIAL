11a6ec5ad409e1140c0d4bb462ff90f0c76c09db
Erwan de Belleville <edebelleville@hotmail.fr>
2022-02-07 10:57:57 +0100

    add likepost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -73,6 +73,31 @@ module.exports.likePost = async (req, res) => {
     if(!ObjectID.isValid(req.params.id))
     return res.status(400).send('ID inconnu' + req.params.id)
 
+    try{
+        await postModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $addToSet : {likers : req.body.id}
+            },
+            {new : true},
+            (err, docs) => {
+                if (err) return res.status(400).send(err)
+            }
+        )
+        await userModel.findByIdAndUpdate(
+            req.body.id,
+            {
+                $addToSet : {likes: req.params.id}
+            },
+            {new : true},
+            (err, docs) => {
+                if(!err) res.send(docs);
+                else return res.status(400).send(err)
+            }
+        )
+    } catch(err) {
+        return res.status(401).send(err)
+    }
 
 }
 
