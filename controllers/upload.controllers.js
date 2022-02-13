04055973bb0cb8b78db2bb5343e0372a57f03111
Erwan de Belleville <edebelleville@hotmail.fr>
2022-02-13 18:24:54 +0100

    modifs upload

--- controllers/upload.controllers.js
+++ controllers/upload.controllers.js
@@ -8,23 +8,26 @@ const pipeline = promisify(require('stream').pipeline);
 
 module.exports.uploadProfil = async (req, res) => {
     try{
-        if(
+        if (
             req.file.dectectedMimeType !== "image/jpg" &&
-            req.file.dectectedMimeType !== "image/jpeg" &&
-            req.file.dectectedMimeType !== "image/png"
-        )
+            req.file.dectectedMimeType !== "image/png" &&
+            req.file.dectectedMimeType !== "image/jpeg"
+        ){
             throw Error("Format du fichier invalide");
-
-        if(req.file.size > 5000000){
+        }
+            
+        if(req.file.size > 500000){
             throw Error("max size");
         } 
+       
     }
     catch(err){
         const errors = upLoadErrors(err);
+        console.log("error from the catch", err)
         return res.status(201).json({ errors });
     }
 
-    const fileName = req.body.name + ".jpg";
+    const fileName = req.body.prenom + ".jpg";
 
     await pipeline(
         req.file.stream,
@@ -32,6 +35,4 @@ module.exports.uploadProfil = async (req, res) => {
             `${__dirname}/../client/public/uploads/profil/${fileName}`
         )
     )
-
-
 };
